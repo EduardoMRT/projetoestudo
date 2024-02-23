cc905c7115147cc04cae3b292019f938b18a17ce
EduardoMRT <eduksoficial@gmail.com>
2024-02-23 12:26:46 -0300

    update

--- Mercadinho/src/br/com/eduardo/mercadinho/verificador/VerificadorCliente.java
+++ Mercadinho/src/br/com/eduardo/mercadinho/verificador/VerificadorCliente.java
@@ -3,12 +3,12 @@ package br.com.eduardo.mercadinho.verificador;
 import br.com.eduardo.mercadinho.util.Cliente;
 
 public class VerificadorCliente {
-    public String verificaIdade(Cliente cliente){
+    public String verificaIdade(Cliente cliente) {
         int idade = cliente.getIdade();
-            if(idade < 18){
-                return "O cliente é menor de idade";
-            }else{
-                return "O cliente é maior de idade";
-            }
+        if (idade < 18) {
+            return "O cliente é menor de idade";
+        } else {
+            return "O cliente é maior de idade";
+        }
     }
 }
